da27bc0855c0af56adc123c07c1c62bd12c5017c
Mike Chaberski <mchaberski@novetta.com>
2016-09-01 11:57:14 -0400

    be tolerant of not-pre-existing database in integration test

--- example/src/test/java/com/github/mike10004/mysqldistuser/DatabaseClientIT.java
+++ example/src/test/java/com/github/mike10004/mysqldistuser/DatabaseClientIT.java
@@ -3,6 +3,9 @@
  */
 package com.github.mike10004.mysqldistuser;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
@@ -14,7 +17,14 @@ public class DatabaseClientIT {
 
     @Test
     public void doSomething() throws Exception {
-        String jdbcUrl = "jdbc:mysql://localhost:" + resolvePort() + "/test";
+        String jdbcUrl = "jdbc:mysql://localhost:" + resolvePort() + "/";
+        String user = "root", password = "root";
+        String databaseName = "DatabaseClientIT";
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, user, password);
+                Statement stmt = conn.createStatement()) {
+            stmt.execute("CREATE DATABASE IF NOT EXISTS " + databaseName);
+        }
+        jdbcUrl = jdbcUrl + databaseName;
         DatabaseClient client = new DatabaseClient(jdbcUrl, "root", "root");
         Object result = client.doSomething();
         System.out.println("result: " + result);
